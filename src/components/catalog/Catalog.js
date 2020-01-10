da3b6a5e81f41df58b2c5896cf342768fd633a63
lmeikle <laura.s.meikle@gmail.com>
2020-01-10 12:18:03 +0000

    use ems for sizing

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -4,19 +4,22 @@ import styles from './catalog.module.css';
 import Item from './item/Item';
 
 const KAWAII_CHARACTERS = [
-  <kawaii.Backpack size={200} mood="blissful" color="#FCCB7E" />,
-  <kawaii.Cat size={200} mood="lovestruck" color="#A6E191" />,
-  <kawaii.File size={200} mood="happy" color="#FDA7DC" />,
-  <kawaii.Ghost size={200} mood="excited" color="#E0E4E8" />,
-  <kawaii.IceCream size={200} mood="sad" color="#83D1FB" />,
-  <kawaii.Mug size={200} mood="shocked" color="#FCCB7E" />,
-  <kawaii.Planet size={200} mood="ko" color="#A6E191" />,
-  <kawaii.SpeechBubble size={200} mood="blissful" color="#FDA7DC" />,
+  <kawaii.Backpack mood="blissful" color="#FCCB7E" />,
+  <kawaii.Cat mood="lovestruck" color="#A6E191" />,
+  <kawaii.File mood="happy" color="#FDA7DC" />,
+  <kawaii.Ghost mood="excited" color="#E0E4E8" />,
+  <kawaii.IceCream mood="sad" color="#83D1FB" />,
+  <kawaii.Mug mood="shocked" color="#FCCB7E" />,
+  <kawaii.Planet mood="ko" color="#A6E191" />,
+  <kawaii.SpeechBubble mood="blissful" color="#FDA7DC" />,
+  <kawaii.Browser mood="lovestruck" color="#E0E4E8" />,
+  <kawaii.CreditCard mood="happy" color="#83D1FB" />,
 ];
 
 const Catalog = () => {
   return (
     <div className={styles.container}>
+      <span className={styles.title}>Catalog of Characters</span>
       <div className={styles.itemsContainer}>
         {KAWAII_CHARACTERS.map(val => {
           return <Item character={val} />;
